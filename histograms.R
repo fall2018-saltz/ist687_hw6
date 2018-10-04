17f850af5ce597a31c734c905b41f85a8850ec5f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:18:27 -0400

    Saving histograms.R

--- histograms.R
+++ histograms.R
@@ -11,5 +11,6 @@ histMurderRate <-hist(df$Murder,  main = "Histogram of State Populations",
     xlab = "Population" , ylab="Frequency",col = 'orange',border = 'red',density = 100,
     breaks=20 )
 
-histpop<- ggplot(df,aes(x=population,fill=orange))+
-             geom_histogram()
+histpop<- 
+ggplot(dfStates,aes(x=population,fill=Red))+
+  geom_histogram(bins=5,color= "blue",fill="white")

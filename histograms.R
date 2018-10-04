af047a4889ec98b3b44ec2a88b0c4b8794a04a9e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:04:25 -0400

    Saving histograms.R

--- histograms.R
+++ histograms.R
@@ -5,6 +5,6 @@ histpop <-hist(df$population,  main = "Histogram of State Populations",
     xlab = "Population" , ylab="Frequency",col = 'orange',border = 'red',density = 100,
     breaks=20 )
 
-histpop <-hist(df$Murder,  main = "Histogram of State Populations",
+histMurderRate <-hist(df$Murder,  main = "Histogram of State Populations",
     xlab = "Population" , ylab="Frequency",col = 'orange',border = 'red',density = 100,
     breaks=20 )

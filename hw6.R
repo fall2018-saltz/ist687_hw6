0be016066e8d51989dbab072a950199714b87260
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:16:41 -0400

    Saving hw6.R

--- hw6.R
+++ hw6.R
@@ -1,8 +1,8 @@
 
-setwd("D:/ADS/IST 687/HW3")
-dfStates <- read.csv(file="scprc-est2017-18+pop-res.csv", stringsAsFactors= FALSE)
-dfStates<- dfStates[c(-1,-53),-1:-4]
-colnames(dfStates)<-c("stateName","population","popOver18","percentOver18")
-return(dfStates)
+#setwd("D:/ADS/IST 687/HW3")
+#dfStates <- read.csv(file="scprc-est2017-18+pop-res.csv", stringsAsFactors= FALSE)
+#dfStates<- dfStates[c(-1,-53),-1:-4]
+#colnames(dfStates)<-c("stateName","population","popOver18","percentOver18")
+#return(dfStates)
 
 census

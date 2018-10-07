6350f687db43276513a1df432b50b9cb57617f75
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 21:54:16 -0400

    Saving MaxMurder.R

--- MaxMurder.R
+++ MaxMurder.R
@@ -6,15 +6,15 @@ library(dplyr)
 df<- final_data
 #str(df)
 df$NumMurder <- as.integer(df$Murder*df$population/100000)
-str(df)
-df1<-df[order(df$NumMurder),]
-str(df1)
+#str(df)
+
+#str(df1)
 #barchart 
 barNumMurder1<-ggplot(df,aes(x=stateName,y=NumMurder))+
     geom_col()+
     theme(axis.text.x = element_text(angle = 90))#+
     #ggtitle(“Total Murders”)
-
+df1<-df[order(df$NumMurder),]
 barNumMurder2<-ggplot(df1,aes(x=stateName,y=NumMurder))+
     geom_col()+
     theme(axis.text.x = element_text(angle = 90))#+

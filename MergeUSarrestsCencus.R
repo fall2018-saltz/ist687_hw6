20b9249445d56b99a1482b83e4a90be231b4d956
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 21:39:04 -0400

    Saving MergeUSarrestsCencus.R

--- MergeUSarrestsCencus.R
+++ MergeUSarrestsCencus.R
@@ -1,6 +1,7 @@
 
 arrests <- USArrests
 stateName<-rownames(arrests)
-final_data <-merge(clean_data,arrests,by="stateName")
-str(final_data)
-str(clean_data)
+#final_data <-merge(clean_data,arrests,by="stateName")
+#str(final_data)
+str(arrests)
+

aa4fd595dbc2899627be15b3dc684ac41b4c97dc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 20:41:52 -0400

    Saving MaxMurder.R

--- MaxMurder.R
+++ MaxMurder.R
@@ -10,4 +10,5 @@ str(df)
 #barchart 
 barNumMurder<-ggplot(df,aes(x=stateName,y=NumMurder))+
     geom_col()+
-     theme(axis.text.x = element_text(angle = 90))+
+    theme(axis.text.x = element_text(angle = 90))+
+    ggtitle(“Total Murders”)

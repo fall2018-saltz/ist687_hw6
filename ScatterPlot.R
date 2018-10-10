2171f3bbac1335cb3328433541fa0228e17822e7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:28:21 -0400

    Saving ScatterPlot.R

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -2,5 +2,5 @@
 df<-final_data
 library(ggplot2)
 
-scatplot <- ggplot(df,aes(x=population,y=percentOver18))+
+scatplot <- ggplot(df,aes(x=population,y=percentOver18,size=Murder,col=Murder))+
     geom_point()

fa47de64c114f5ee6ba8ff05a1f3453312348da2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:03:10 -0400

    Saving MaxMurder.R

--- MaxMurder.R
+++ MaxMurder.R
@@ -22,7 +22,7 @@ barNumMurder2<-ggplot(df,aes(x=reorder(stateName,NumMurder),y=NumMurder))+
     #ggtitle(“Total Murders”)
     
 
-barNumMurder3<-ggplot(df1,aes(x=stateName,y=NumMurder,fill=percentOver18))+
+barNumMurder3<-ggplot(df,aes(x=stateName,y=NumMurder,fill=percentOver18))+
     geom_col()+
     theme(axis.text.x = element_text(angle = 90))#+
     #ggtitle(“Total Murders”)

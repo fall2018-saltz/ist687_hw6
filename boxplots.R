0792484fe0dd1c5004ae6ee951d60f6a2a45466d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:29:01 -0400

    Saving boxplots.R

--- boxplots.R
+++ boxplots.R
@@ -17,4 +17,5 @@ ggplot(df,aes(x=factor(0),y=population))+
 #geom_boxplot plots the box plot. 
 
 # histogram vs boxplot
-# According to me, Histogram was more powerful,it gives me freedom of selecting how the histogram will look 
+# According to me, Histogram was more powerful,it gives me freedom of selecting how the histogram will look, 
+# in terms of the grouping of the data

d01cc8d72f6b0d5c2e032d00261e2516fc8d9c8a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:18:52 -0400

    Saving histograms.R

--- histograms.R
+++ histograms.R
@@ -25,4 +25,5 @@ ggplot(df,aes(x=UrbanPop))+
     geom_histogram(color='blue',fill='white',bins=8)
     
 #ggplot function forms an object with dataframe and variables 
-#the second line, geom_histogram plots the histogram with colour of edge, fill and, number of bins are specified with it
+#the second line, geom_histogram plots the histogram with colour of edge, fill and,
+#number of bins are specified along with it.

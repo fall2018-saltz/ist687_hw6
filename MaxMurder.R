451e3b4b8a6d26f44898a283ec6dfc50d17e49d9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:34:34 -0400

    Saving MaxMurder.R

--- MaxMurder.R
+++ MaxMurder.R
@@ -3,19 +3,19 @@
 library(ggplot2)
 df<- final_data
 
-# calculating the 
+# calculating the number of murders per state by multiplying the murder rate per 100000 population into the actual population
 df$NumMurder <- as.integer(df$Murder*df$population/100000)
 
 #barchart 
 barNumMurder1<-ggplot(df,aes(x=stateName,y=NumMurder))+
     geom_col()+
     theme(axis.text.x = element_text(angle = 90))#+
-    #ggtitle(“Total Murders”)ead(df1)
+    #ggtitle(“Total Murders”)
+    
 barNumMurder2<-ggplot(df,aes(x=reorder(stateName,Murder),y=NumMurder))+
     geom_col()+
     theme(axis.text.x = element_text(angle = 90))#+
     #ggtitle(“Total Murders”)
-    
 
 barNumMurder3<-ggplot(df,aes(x=reorder(stateName,Murder),y=NumMurder,fill=percentOver18))+
     geom_col()+

e9eaf867dd2cb58d7105ab8d0ad9e4f40445745b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:07:25 -0400

    Saving MergeUSarrestsCencus.R

--- MergeUSarrestsCencus.R
+++ MergeUSarrestsCencus.R
@@ -1,6 +1,7 @@
 
 #copying USarrests data in a variable
 arrests <- USArrests
+#creating a row named stateName
 arrests$stateName<-rownames(arrests)
 final_data <-merge(clean_data,arrests,by="stateName")
 str(final_data)

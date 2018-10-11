5c3d7fc89be1d8e43c95f828beb7a5895c367c60
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:39:42 -0400

    Saving MaxMurder.R

--- MaxMurder.R
+++ MaxMurder.R
@@ -7,14 +7,13 @@ df<- final_data
 #the murder rate per 100000 population into the actual population of the state and taking it as integer because murders should be in whole numbers.
 df$NumMurder <- as.integer(df$Murder*df$population/100000)
 
-#barchart 
-
+#barchart with the number of murders per state
 barNumMurder1<-ggplot(df,aes(x=stateName,y=NumMurder))+
     geom_col()
- 
+#adding theme function to rotate text of x axis by 90 degree 
 barNumMurder2<-ggplot(df,aes(x=stateName,y=NumMurder))+
     geom_col()    
-    #theme(axis.text.x = element_text(angle = 90))#+
+    theme(axis.text.x = element_text(angle = 90))#+
     #ggtitle(“Total Murders”)
     
 barNumMurder3<-ggplot(df,aes(x=reorder(stateName,Murder),y=NumMurder))+

15c7d314b1958d6daf844c300ac1790bafbe9ab5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:25:00 -0400

    Saving boxplots.R

--- boxplots.R
+++ boxplots.R
@@ -13,4 +13,5 @@ boxpop<-
 ggplot(df,aes(x=factor(0),y=population))+
   geom_boxplot(color= "blue",fill="white")
 
-#ggplot creates the object here x variable is set to 0 as it is required
+#ggplot creates the object, here x variable is set to 0 as it is required
+#geom_boxplot plots the box plot. 
